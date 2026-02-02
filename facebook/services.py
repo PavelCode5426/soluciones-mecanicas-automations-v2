9a0174e3fe9d88d9c41540e1b6e4d7fba8c511f6
PavelCode5426 <perezpavel5426@gmail.com>
2026-02-02 08:48:19 -0500

    Init

--- facebook/services.py
+++ facebook/services.py
@@ -106,7 +106,8 @@ class FacebookAutomationService:
                 page.keyboard.insert_text(post.text)
 
                 time.sleep(random.randint(30, 60))
-                page.click('[aria-label="Publicar"]')
+                # page.click('[aria-label="Publicar"]')
+                page.get_by_text('Publicar').click()
                 page.get_by_text("Publicando", exact=True).wait_for(state='hidden')
 
             except Exception as e:
