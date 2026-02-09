a8c8ada78d17f1ed11bf50d15093380fc5423ce6
PavelCode5426 <perezpavel5426@gmail.com>
2026-02-08 19:29:35 -0500

    Update admin.py

--- facebook/services.py
+++ facebook/services.py
@@ -121,7 +121,8 @@ class FacebookAutomationService:
                 time.sleep(random.randint(30, 60))
 
                 page.click('[aria-label="Publicar"]')
-                page.get_by_text("Publicando").wait_for(state='hidden')
+                page.locator('span', has_text='Publicando').wait_for(state='hidden')
+                # page.get_by_text("Publicando").wait_for(state='hidden')
 
             except Exception as e:
                 exception = e
