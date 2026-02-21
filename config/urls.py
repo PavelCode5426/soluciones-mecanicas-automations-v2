69117bebe06d43a6110d2c3b5ada2abe90b47b54
PavelCode5426 <perezpavel5426@gmail.com>
2026-02-21 01:07:16 -0500

    Init whatsapp response

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
                   path("admin/", admin.site.urls),
-                  include('facebook.urls', namespace='facebook'),
+                  path("", include('facebook.urls', namespace='facebook')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- facebook/urls.py
+++ facebook/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from facebook import views
 
+app_name = 'facebook'
+
 urlpatterns = [
     path('whatsapp-message', views.WhatsAppMessageWebhookView.as_view(), name='whatsapp-webhook'),
 ]
