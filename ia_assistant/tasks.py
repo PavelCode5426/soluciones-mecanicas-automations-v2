96c43abe132a0395725d7b6ea19f1af8996057c8
PavelCode5426 <perezpavel5426@gmail.com>
2026-03-06 15:52:08 -0500

    Create new ia_assistant module

--- ia_assistant/tasks.py
+++ ia_assistant/tasks.py
@@ -11,6 +11,17 @@ from services import WAHAService
 AGENTS_FUNCTION = {}
 
 
+def get_keep_typing_loop_task(service: WAHAService, account_id):
+    async def __keep_typing():
+        try:
+            while True:
+                service.start_typing(account_id)
+                await asyncio.sleep(5)
+        except asyncio.CancelledError:
+            service.stop_typing(account_id)
+    return asyncio.create_task(__keep_typing())
+
+
 def reply_whatsapp_message(agent_name, message, account_id):
     agent = cache.get_or_set(agent_name, Agent.objects.get(name=agent_name))
     whatsapp_service = WAHAService.initialize_using_config()
@@ -21,29 +32,22 @@ def reply_whatsapp_message(agent_name, message, account_id):
         func_agent = service.get_agent(agent)
         AGENTS_FUNCTION.setdefault(agent_name, func_agent)
 
-    async def keep_typing():
-        try:
-            while True:
-                whatsapp_service.start_typing(account_id)
-                await asyncio.sleep(5)
-        except asyncio.CancelledError:
-            whatsapp_service.stop_typing(account_id)
-
     async def main():
         previus_context = cache.get_or_set(account_id, {})
         ctx = Context(func_agent, previous_context=previus_context)
-        typing_task = asyncio.create_task(keep_typing())
+        typing_task = get_keep_typing_loop_task(whatsapp_service, account_id)
         try:
             async with asyncio.timeout(settings.IA_TIMEOUT):
                 result = await func_agent.run(message, ctx=ctx)
                 whatsapp_service.send_text(account_id, str(result))
         finally:
             typing_task.cancel()
-
         cache.set(account_id, ctx.to_dict())
 
     if '--reset' in message:
         cache.delete(account_id)
         whatsapp_service.send_text(account_id, "Memoria borrada....")
     else:
+        # loop = asyncio.get_event_loop()
+        # loop.run_until_complete(main())
         asyncio.run(main(), debug=True)
