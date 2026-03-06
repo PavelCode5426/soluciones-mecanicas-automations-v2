6f9f3a1c811a8aabc9397aad5574a76c2d19c761
PavelCode5426 <perezpavel5426@gmail.com>
2026-03-05 22:19:33 -0500

    Create new ia_assistant module

--- ia_assistant/urls.py
+++ ia_assistant/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from ia_assistant import views
 
-app_name = 'facebook'
+app_name = 'ia_assistant'
 
 urlpatterns = [
     path(r'whatsapp-message/', views.WhatsAppMessageWebhookView.as_view(), name='whatsapp-webhook'),
