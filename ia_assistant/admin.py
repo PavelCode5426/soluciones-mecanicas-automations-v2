1e9bc1646042721f82660fd32133005d835999b8
PavelCode5426 <perezpavel5426@gmail.com>
2026-03-05 23:39:52 -0500

    Create new ia_assistant module

--- ia_assistant/admin.py
+++ ia_assistant/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from ia_assistant.models import Agent
+from ia_assistant.models import Agent, AgentTool
+
 
 # Register your models here.
 @admin.register(Agent)
 class AgentAdmin(admin.ModelAdmin):
     list_display = ['name', 'description']
+
+
+@admin.register(AgentTool)
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ['agent_name', 'name', 'description']
+
+    def agent_name(self, obj):
+        return obj.agent.name
+
+    agent_name.admin_order_field = 'agent'
+    agent_name.short_description = 'Agente'
