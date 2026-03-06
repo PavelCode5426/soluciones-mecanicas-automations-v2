313065c90ed195fd8c48168486e832914d2cf266
PavelCode5426 <perezpavel5426@gmail.com>
2026-03-05 22:21:59 -0500

    Create new ia_assistant module

--- ia_assistant/admin.py
+++ ia_assistant/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from ia_assistant.models import Agents
+from ia_assistant.models import Agent
 
 # Register your models here.
-admin.site.register(Agents)
+admin.site.register(Agent)
--- ia_assistant/models.py
+++ ia_assistant/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Agents(models.Model):
+class Agent(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField(null=True, blank=True)
     system_prompt = models.TextField(null=True, blank=True)
