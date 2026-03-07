53eef9dd938abe782179e9ad5a6399b2214644a2
PavelCode5426 <perezpavel5426@gmail.com>
2026-03-07 13:58:54 -0500

    Init agent workflow

---
+++ ia_assistant/management/commands/ia.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from django.conf import settings
+from django.core.cache import cache
+from django.core.management.base import BaseCommand
+from llama_index.core.agent import AgentStream
+from llama_index.core.workflow import Context
+
+from ia_assistant.models import Agent
+from ia_assistant.services import SolucionesHeviaIAService
+
+
+class Command(BaseCommand):
+    help = "Hablar con la IA"
+    agent_name = 'seller_agent'
+
+    def handle(self, *args, **options):
+        agent = cache.get_or_set(self.agent_name, Agent.objects.get(name=self.agent_name))
+        func_agent = SolucionesHeviaIAService().get_agent(agent)
+
+        async def main():
+            ctx = Context(func_agent)
+            while True:
+                query = input("Tu:")
+                if query == "q":
+                    break
+                if query != "":
+                    handler = agent.run(query, ctx=ctx)
+                    async for event in handler.stream_events():
+                        if isinstance(event, AgentStream):
+                            print(event.delta, end="", flush=True)
+
+        asyncio.run(main(), debug=True)
