f1fff2f50083f4fdcb9e9e6a0dc34d435385f6ad
PavelCode5426 <perezpavel5426@gmail.com>
2026-04-06 14:20:01 -0400

    Fix issues

--- services/automations.py
+++ services/automations.py
@@ -130,7 +130,7 @@ class FacebookAutomationService:
                         textarea = article.get_by_role('textbox')
                         try:
 
-                            response = run_async(post_analyser.run(raw_html=article.inner_html(), ctx=ctx))
+                            response = run_async(post_analyser.run(raw_html=article.inner_html()))
                             print(response.promotional_message)
                             if response.is_relevant:
                                 textarea.click()
