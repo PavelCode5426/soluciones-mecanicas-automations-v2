2bbdb4a55a744402f79d5232a8dada7e403f2099
PavelCode5426 <perezpavel5426@gmail.com>
2026-04-07 15:00:01 -0400

    Add whatsapp lead explorer.

--- services/agents.py
+++ services/agents.py
@@ -202,7 +202,6 @@ class WhatsAppLeadAnalyzer(SocialNetworkAnalyzerAgent):
                                                PromptTemplate(self.system_prompt),
                                                messages=messages, groups=groups, profile=profile)
 
-        print(response)
         return AnalyzerResponseEvent(is_relevant=response.is_relevant, justification=response.justification,
                                      promotional_message=response.promotional_message)
 
