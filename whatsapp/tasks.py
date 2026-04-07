fbf2313757b453ff681102c29b4ee1535ac6bca2
PavelCode5426 <perezpavel5426@gmail.com>
2026-04-07 15:44:34 -0400

    Add whatsapp lead explorer.

--- whatsapp/tasks.py
+++ whatsapp/tasks.py
@@ -203,7 +203,7 @@ def send_message_to_lead(lead: WhatsAppLead):
             "linkPreview": False,
             "linkPreviewHighQuality": False
         })
-    all_messages.deleted()
+    all_messages.delete()
     WhatsAppProcessedLead.objects.create(chat_id=lead.chat_id, account=lead.account, chat_name=lead.chat_name,
                                          group=lead.group, messages=long_messages,
                                          message_reply=response.promotional_message, processed=True)
