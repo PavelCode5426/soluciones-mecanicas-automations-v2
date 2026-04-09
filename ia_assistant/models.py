e481c99072fee5cf7714b9e7362f3d1f289d8ab8
PavelCode5426 <perezpavel5426@gmail.com>
2026-04-09 14:54:36 -0400

    Add whatsapp lead explorer.

--- ia_assistant/models.py
+++ ia_assistant/models.py
@@ -2,8 +2,6 @@ from django.core.cache import cache
 from django.db import models
 from django_jsonform.models.fields import JSONField
 
-from ia_assistant.factories import create_function_agent, create_agent_workflow
-
 
 # Create your models here.
 class OllamaLLM(models.Model):
