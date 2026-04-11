de6f05f0a45666ae1954ded3fc0efa6c288ea722
PavelCode5426 <perezpavel5426@gmail.com>
2026-04-11 12:26:19 -0400

    Add IA for Sinergia Solutions

--- facebook/schedules.py
+++ facebook/schedules.py
@@ -1,10 +1,8 @@
-import random
-
-from django.db.models import QuerySet, Q, ExpressionWrapper, F, DecimalField
-from django.utils.timezone import now
+from django.db.models import Q, ExpressionWrapper, F, DecimalField
+from django.utils.timezone import localtime
 from django_q.tasks import async_task
 
-from facebook.models import FacebookGroup, FacebookProfile, FacebookPostCampaign, FacebookLeadExplorer
+from facebook.models import FacebookProfile, FacebookPostCampaign, FacebookLeadExplorer
 from facebook.tasks import enqueue_lead_explorer, enqueue_facebook_campaign
 from services.automations import FacebookAutomationService
 
@@ -25,10 +23,10 @@ def enqueue_active_lead_explorers():
 
 
 def enqueue_active_facebook_campaigns():
-    current_hour = now().hour
+    current_hour = localtime().hour
     posts = (FacebookPostCampaign.objects.select_related('profile')
-             .filter(active=True, from_date__lte=now())
-             .filter(Q(until_date__gte=now()) | Q(until_date__isnull=True))
+             .filter(active=True, from_date__lte=localtime())
+             .filter(Q(until_date__gte=localtime()) | Q(until_date__isnull=True))
              .annotate(can_publish=ExpressionWrapper(F('frequency') % current_hour, DecimalField()))
              .filter(can_publish=0).order_by('?').all())
 
--- whatsapp/schedules.py
+++ whatsapp/schedules.py
@@ -1,11 +1,6 @@
-import datetime
-from datetime import datetime
-
-from dateutil.utils import today
 from django.db.models import Q
-from django.utils.timezone import now
-from django_q.models import Schedule, Task
-from django_q.tasks import async_task, schedule
+from django.utils.timezone import localtime
+from django_q.tasks import async_task
 
 from whatsapp.models import WhatsAppStatus, WhatsAppAccount, WhatsAppMessage
 from whatsapp.tasks import publish_whatsapp_status, syncronize_whatsapp_account_groups, \
@@ -22,9 +17,9 @@ def update_whatsapp_contacts_and_groups():
 
 def enqueue_active_status():
     whatsapp_status = (WhatsAppStatus.objects.select_related('account')
-                       .filter(active=True, from_date__lte=now())
-                       .filter(Q(until_date__gte=now()) | Q(until_date__isnull=True))
-                       .filter(publish_at=now(), weekdays__day=now().weekday())
+                       .filter(active=True, from_date__lte=localtime())
+                       .filter(Q(until_date__gte=localtime()) | Q(until_date__isnull=True))
+                       .filter(publish_at=localtime(), weekdays__day=localtime().weekday())
                        .all())
 
     for status in whatsapp_status:
@@ -32,7 +27,7 @@ def enqueue_active_status():
             publish_whatsapp_status, status,
             task_name=f'create_whatsapp_status_{status.pk}',
             cluster='whatsapp',
-            # next_run=datetime.combine(now(), status.publish_at)
+            # next_run=datetime.combine(localtime(), status.publish_at)
         )
 
     return f"Programados {whatsapp_status.count()} estados de whatsapp"
@@ -40,9 +35,9 @@ def enqueue_active_status():
 
 def enqueue_active_messages():
     messages = (WhatsAppMessage.objects.select_related('account')
-                .filter(active=True, from_date__lte=now())
-                .filter(Q(until_date__gte=now()) | Q(until_date__isnull=True))
-                .filter(publish_at__hour=now().hour, weekdays__day=now().weekday())
+                .filter(active=True, from_date__lte=localtime())
+                .filter(Q(until_date__gte=localtime()) | Q(until_date__isnull=True))
+                .filter(publish_at__hour=localtime().hour, weekdays__day=localtime().weekday())
                 .all())
 
     for message in messages:
