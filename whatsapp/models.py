fe719b37bf107b52f001247b99c80d9da13404c8
PavelCode5426 <perezpavel5426@gmail.com>
2026-04-18 23:24:02 -0400

    Update enqueue_active_messages

--- whatsapp/models.py
+++ whatsapp/models.py
@@ -143,6 +143,9 @@ class WhatsAppStatus(AbstractWhatsAppMessage, ScheduledMessage):
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = 'Estado'
         verbose_name_plural = 'Estados'
