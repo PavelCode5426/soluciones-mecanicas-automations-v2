1f6ba690bb0a7a927a02025d4eafc4a2470381ec
PavelCode5426 <perezpavel5426@gmail.com>
2026-04-21 16:55:38 -0400

    Update and Fix issues

--- whatsapp/static/js/sort-messages.js
+++ whatsapp/static/js/sort-messages.js
@@ -4,7 +4,7 @@ if (group) {
         animation: 150, multiDrag: true, selectedClass: 'active', onEnd: function () {
             var items = group.children;
             for (var i = 0; i < items.length; i++) {
-                var input = items[i].querySelector('[name$="-ORDER"]');
+                var input = items[i].querySelector('input[name$="-ORDER"]');
                 if (input) {
                     input.value = i; // or i+1 for 1-indexed
                 }
