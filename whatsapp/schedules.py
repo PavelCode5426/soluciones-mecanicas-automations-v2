0a0f146bc432184cd021ccd608e7d8177d078c81
PavelCode5426 <perezpavel5426@gmail.com>
2026-04-21 16:36:27 -0400

    Update and Fix issues

--- whatsapp/schedules.py
+++ whatsapp/schedules.py
@@ -1,9 +1,8 @@
 from django.db.models import Q, F, ExpressionWrapper, DecimalField
 from django.utils.timezone import localtime
-from django_q.tasks import async_task
 
 from whatsapp.models import WhatsAppStatus, WhatsAppAccount, WhatsAppMessage
-from whatsapp.tasks import publish_whatsapp_status, syncronize_whatsapp_account_groups, \
+from whatsapp.tasks import syncronize_whatsapp_account_groups, \
     syncronize_whatsapp_account_contacts, enqueue_whatsapp_message, enqueue_whatsapp_status
 
 
@@ -17,8 +16,7 @@ def update_whatsapp_contacts_and_groups():
 
 def enqueue_active_status():
     whatsapp_status = (WhatsAppStatus.objects.select_related('account')
-                       .exclude(account__active=False)
-                       .filter(active=True, from_date__lte=localtime())
+                        .filter(active=True, account__active=True, from_date__lte=localtime())
                        .filter(Q(until_date__gte=localtime()) | Q(until_date__isnull=True))
                        # .filter(publish_at=localtime(), weekdays__day=localtime().weekday())
                        .filter(weekdays__day=localtime().weekday())
@@ -36,8 +34,8 @@ def enqueue_active_messages():
     current_weekday = current_time.weekday()
 
     messages = (WhatsAppMessage.objects.select_related('account')
-                .exclude(frequency__isnull=True, account__active=False)
-                .filter(active=True, from_date__lte=current_time)
+                .exclude(frequency__isnull=True)
+                .filter(active=True, account__active=True, from_date__lte=current_time)
                 .filter(Q(until_date__gte=current_time) | Q(until_date__isnull=True))
                 .filter(from_time__lte=current_time, until_time__gte=current_time)
                 .filter(weekdays__day=current_weekday)
--- whatsapp/static/js/sort-messages.js
+++ whatsapp/static/js/sort-messages.js
@@ -1,10 +1,7 @@
 // sort-messages.js
-var tbody = document.querySelector('#sortable-table tbody');
-if (tbody) {
-    new Sortable(tbody, {
-        animation: 150,
-        multiDrag: true,
-        selectedClass: 'bg-secondary',
-        fallbackTolerance: 3,
+var group = document.querySelector('#sortable-list');
+if (group) {
+    new Sortable(group, {
+        animation: 150, multiDrag: true, selectedClass: 'active'
     });
 }
