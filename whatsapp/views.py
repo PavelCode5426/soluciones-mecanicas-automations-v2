ad11850788e408a3ce89a8bc4a5f0075e8ee48bd
PavelCode5426 <perezpavel5426@gmail.com>
2026-04-21 17:56:53 -0400

    Update and Fix issues

--- whatsapp/views.py
+++ whatsapp/views.py
@@ -293,7 +293,7 @@ class WhatsAppMessagesSorterView(SuccessMessageMixin, FormView):
         return self.account.messages.all()
 
     def get_context_data(self, **kwargs):
-        return super().get_context_data(object_list=self.get_queryset(), **kwargs)
+        return super().get_context_data(object_list=self.get_queryset(), account=self.account, **kwargs)
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
