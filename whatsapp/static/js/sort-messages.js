90a33b0bae653435bf5a9f1d11aafc60e589e6d4
PavelCode5426 <perezpavel5426@gmail.com>
2026-04-21 16:45:16 -0400

    Update and Fix issues

--- whatsapp/static/js/sort-messages.js
+++ whatsapp/static/js/sort-messages.js
@@ -1,7 +1,14 @@
-// sort-messages.js
 var group = document.querySelector('#sortable-list');
 if (group) {
     new Sortable(group, {
-        animation: 150, multiDrag: true, selectedClass: 'active'
+        animation: 150, multiDrag: true, selectedClass: 'active', onEnd: function () {
+            var items = group.children;
+            for (var i = 0; i < items.length; i++) {
+                var input = items[i].querySelector('[name$="-ORDER"]');
+                if (input) {
+                    input.value = i; // or i+1 for 1-indexed
+                }
+            }
+        }
     });
 }
