e523bf39d84f897241cb6576f862fabde298603a
PavelCode5426 <perezpavel5426@gmail.com>
2026-05-01 00:14:26 -0400

    Update facebook agent

--- services/automations.py
+++ services/automations.py
@@ -147,7 +147,8 @@ class FacebookAutomationService:
                     print(f"Agent error: {e}")
 
             explorer.leads_found = F('leads_found') + leads_found
-            explorer.save(update_fields=['leads_found'])
+            explorer.active = False
+            explorer.save(update_fields=['leads_found', 'active'])
 
     def save_session(self, storage_state):
         self.profile.context = storage_state
