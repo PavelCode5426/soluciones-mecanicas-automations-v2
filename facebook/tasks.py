93e15f6a00b929ef9cd6fff50eb6e10da34da25c
PavelCode5426 <perezpavel5426@gmail.com>
2026-05-04 02:36:12 -0400

    Update Facebook to V2

--- facebook/tasks.py
+++ facebook/tasks.py
@@ -33,7 +33,6 @@ def enqueue_lead_explorer(explorer: FacebookAgent):
     if explorer.distribution_list:
         real_account = real_account.filter(groups__group__distribution_lists=explorer.distribution_list)
     real_account = real_account.order_by('?').first()
-
     service = RealAccountAutomationService(real_account)
     task_name = f"agent_{explorer}".lower().replace(" ", "_")
     group_name = f'run_agent_{explorer.profile}'.lower().replace(" ", '_')
